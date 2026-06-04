ee33047e216be9e08021bfead57c843aacd786d9
14247074 <14247074@205-11.Fortis.local>
2026-06-04 12:28:21 +0200

    alle listen sind da und müssten funktionieren oder so 5

--- src/zoo/Person.java
+++ src/zoo/Person.java
@@ -1,14 +1,52 @@
 package zoo;
 
+import java.util.ArrayList;
+
 public class Person {
 
-    boolean Eurasien, Afrika, Nordamerika, Südamerika, AustralienUndOzeanien, TropischesZentrum, Meereswelt;
-    int alter, auftragsnummer; //hallo
+    public static ArrayList<String> bereiche = new ArrayList<>();
+    public static ArrayList<Boolean> BooleanList = new ArrayList<>();
+    public static ArrayList<Integer> PreisErwachsen = new ArrayList<>();
+    public static ArrayList<Integer> PreisKind = new ArrayList<>();
 
-    public Person(int Alter, int Auftragsnummer) {
+    static {
+        bereiche.add("Eurasien");
+        bereiche.add("Afrika");
+        bereiche.add("Nordamerika");
+        bereiche.add("Südamerika");
+        bereiche.add("AustralienUndOzeanien");
+        bereiche.add("TropischesZentrum");
+        bereiche.add("Meereswelt");
+
+        BooleanList.add(false);
+        BooleanList.add(false);
+        BooleanList.add(false);
+        BooleanList.add(false);
+        BooleanList.add(false);
+        BooleanList.add(false);
+        BooleanList.add(false);
+
+        PreisErwachsen.add(5);
+        PreisErwachsen.add(10);
+        PreisErwachsen.add(15);
+        PreisErwachsen.add(20);
+        PreisErwachsen.add(25);
+        PreisErwachsen.add(30);
+        PreisErwachsen.add(35);
+
+        PreisKind.add(2);
+        PreisKind.add(5);
+        PreisKind.add(7);
+        PreisKind.add(10);
+        PreisKind.add(12);
+        PreisKind.add(15);
+        PreisKind.add(17);
+
+    }
+    int alter, auftragsnummer;
+
+    public Person(int alter, int auftragsnummer) {
         this.alter = alter;
         this.auftragsnummer = auftragsnummer;
     }
 }
-
-// 4 Listen: Eine string mit den bereichen eine boolean eine mit breisen erwachsen und eine mit preisen kindern
