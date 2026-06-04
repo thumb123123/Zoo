817b5fc532d47a301a676e5f60e7e43293f6da30
14247074 <14247074@205-11.Fortis.local>
2026-06-04 11:57:00 +0200

    denke das passt so d

--- src/zoo/Person.java
+++ src/zoo/Person.java
@@ -1,5 +1,12 @@
 package zoo;
 
 public class Person {
+
     boolean Eurasien, Afrika, Nordamerika, Südamerika, AustralienUndOzeanien, TropischesZentrum, Meereswelt;
+    int alter, auftragsnummer;
+
+    public Person(int Alter, int Auftragsnummer) {
+        this.alter = alter;
+        this.auftragsnummer = auftragsnummer;
+    }
 }
