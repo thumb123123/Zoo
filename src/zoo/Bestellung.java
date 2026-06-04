0da4c92dde833338fa29e21dfc68617d2c8f8717
verwirrtesBen <14257025@205-10.Fortis.local>
2026-06-04 12:58:25 +0200

    änderung an Preisberechnung

--- src/zoo/Bestellung.java
+++ src/zoo/Bestellung.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
  * @author Ben
  */
 public class Bestellung {
-    public ArrayList<Person> PersonListe = new ArrayList<Person>();
+    public ArrayList<Person> PersonListe = new ArrayList<>();
     public boolean Parkplatz;
+    public int Parkplatzpreis = 5; // Preis in €
+    public int KindAlter = 17;
     // Konstruktor
     public Bestellung(){
         
@@ -19,6 +21,32 @@ public class Bestellung {
     
     public void Personhinzufügen(Person Kunde){
         PersonListe.add(Kunde);
+        System.out.println("person hinzugefügt");
+    }
+    
+    public int getPreis(){
+        System.out.println("presis wird berechnet");
+        int preis=0;
+        for(int i = 0; i < PersonListe.size(); i++){
+            Person Person = this.PersonListe.get(i);
+            int alter = Person.alter;
+            for(int j = 0; j < Person.bereiche.size(); j++){
+                if(Person.bereiche.get(j).isBesucht()==true){
+                    if(alter>KindAlter){
+                        preis += Person.bereiche.get(j).getPreisErwachsen();
+                    }else{
+                        preis += Person.bereiche.get(j).getPreisKind();
+                    }
+                }
+            }
+        }
+
+        // Parkplatz
+        if(this.Parkplatz==true){
+            preis += this.Parkplatzpreis;
+        }
+        System.out.println("preis wurde berechnet");
+        return preis;
     }
     
     
--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -3,6 +3,18 @@ package zoo;
 public class Zoo {
 
     public static void main(String[] args) {
+        // Testcode
+        Bestellung erste = new Bestellung();
+        // Beispielperson
+        Person Bob = new Person(70, 1); // Alter, Auftagsnummer
+        
+        erste.Personhinzufügen(Bob);
+        
+        erste.Parkplatz = true;
+        
+        System.out.println(erste.getPreis());
+        System.out.println("Ende");
+        
 
     }
     
