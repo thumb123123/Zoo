4e1ad28d01930da128ff8f0c1d7c35e6ae3f3d98
14247074 <14247074@205-11.Fortis.local>
2026-06-04 11:42:08 +0200

    neu und so

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -1,17 +1,9 @@
 package zoo;
 
 public class Zoo {
-    
-    static int  sumOff(int a, int b){
-            int result = a + b;
-            return result;
-        }
 
     public static void main(String[] args) {
-        System.out.print(sumOff(1, 5)); // Methodenaufruf
-        // Das ist die Notiz von Ben
-        // ajfsdjioadjf kommentar von sergei zwei mal dies mal klappts
-        // zweiter kommentar von Ben
+
     }
     
 }
