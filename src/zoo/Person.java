25f534c5932429388a4822b6aadbe16fe7de675a
verwirrtesBen <14257025@205-10.Fortis.local>
2026-06-04 12:03:55 +0200

    anleitung

--- src/zoo/Person.java
+++ src/zoo/Person.java
@@ -3,3 +3,5 @@ package zoo;
 public class Person {
     boolean Eurasien, Afrika, Nordamerika, Südamerika, AustralienUndOzeanien, TropischesZentrum, Meereswelt;
 }
+
+// 4 Listen: Eine string mit den bereichen eine boolean eine mit breisen erwachsen und eine mit preisen kindern
